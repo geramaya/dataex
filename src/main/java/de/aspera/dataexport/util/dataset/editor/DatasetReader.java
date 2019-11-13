75b82fa7d5ab8e99fbaa67af31d595f0c852f2c2
Ahmed.Osman <ahmed.osman@aspera.com>
2019-11-13 09:09:14 +0100

    New: Dataset Reader + Groovy Dependencies COMDEV-463

---
+++ src/main/java/de/aspera/dataexport/util/dataset/editor/DatasetReader.java
@@ -0,0 +1,68 @@
+package de.aspera.dataexport.util.dataset.editor;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dbunit.dataset.Column;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.ITableMetaData;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+
+public class DatasetReader {
+	private IDataSet dataset;
+	private Map<String, ITable> tablesMap;
+
+	public DatasetReader() {
+	}
+
+	public void readDataset(String filePath) throws Exception {
+		this.dataset = new FlatXmlDataSetBuilder().build(new FileInputStream(filePath));
+		buildTableMap();
+	}
+
+	public List<String> getTabelNames() throws DataSetException {
+		return new ArrayList<String>(tablesMap.keySet());
+	}
+
+	public int getRowCountOfTable(String tableName) {
+		return this.tablesMap.get(tableName).getRowCount();
+	}
+
+	public void setDataset(IDataSet dataset) throws Exception {
+		this.dataset = dataset;
+		buildTableMap();
+	}
+
+	public List<String> getColumnNamesOfTable(String tableName) throws DataSetException {
+		Column[] cols = tablesMap.get(tableName).getTableMetaData().getColumns();
+		List<String> colNames = new ArrayList<String>();
+		for (int i = 0; i < cols.length; i++) {
+			colNames.add(cols[i].getColumnName());
+		}
+		return colNames;
+	}
+	private void buildTableMap() throws Exception {
+		if (dataset == null) {
+			//Hier muss noch angepasst
+			throw new Exception();
+		}
+		tablesMap = new HashMap<String, ITable>();
+		String[] tableNames = dataset.getTableNames();
+		for (int i = 0; i < tableNames.length; i++) {
+			ITable table = dataset.getTable(tableNames[i]);
+			tablesMap.put(tableNames[i], table);
+		}
+	}
+	public ITableMetaData getMetaDataOfTable(String tableName) {
+		return tablesMap.get(tableName).getTableMetaData();
+	}
+	public Object getValueInTable(String tableName, int row, String colName) throws DataSetException {
+		return tablesMap.get(tableName).getValue(row, colName);
+	}
+
+}
